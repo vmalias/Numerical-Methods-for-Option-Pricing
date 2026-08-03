1761cd18a02bb38a7aace88510b39c4f36221938
V Maliachoustas <v.maliachoustas@gmail.com>
2026-08-03 19:10:28 +0300

    Modified the Black-Scholes related files.

--- Tree-based Methods/BlackScholes/BlackScholesEuCall.R
+++ Tree-based Methods/BlackScholes/BlackScholesEuCall.R
@@ -1,14 +1,22 @@
-#------------------------------------#
-# Black-Scholes option pricing model #
-#      European Call Option          #
-# Inputs: S0,K,T,σ,r                 #
-# Output: Option price               #
-#------------------------------------#
+# Black-Scholes option pricing model / European Call Option 
+
+# K : Strike Price
+# S0 : Price of the underlying asset at time 0
+# T : Maturity
+# sigma : Volatility of the underlying asset
+# r : Risk-free rate                             
+
 
 BlackScholesEuCall <- function(S0,K,T,sigma,r){
-  d1 <- (log(S0/K) + (r + 0.5*sigma^2)*T)/(sigma*sqrt(T))
-  d2 <- d1 - sigma*sqrt(T)
-  C <- S0*pnorm(d1) - K*exp(-r*T)*pnorm(d2)
+  
+  # d1 and d2 are parameters of the Black-Scholes formula
+  d1 = (log(S0/K) + (r + 0.5*sigma^2)*T)/(sigma*sqrt(T))
+  d2 = d1 - sigma*sqrt(T)
+  
+  # Calculate the price of the European option using the Black-Scholes formula
+  C = S0*pnorm(d1) - K*exp(-r*T)*pnorm(d2)
   return(C)
 }
-BlackScholesEuCall(155.87,155.87,3,0.011733586,0.0086)
+
+# An example of inputs:
+# BlackScholesEuCall(155.87,155.87,3,0.011733586,0.0086)
--- Tree-based Methods/BlackScholes/BlackScholesEuPut.R
+++ Tree-based Methods/BlackScholes/BlackScholesEuPut.R
@@ -1,8 +1,23 @@
+# Black-Scholes option pricing model / European Put Option 
+
+# K : Strike Price
+# S0 : Price of the underlying asset at time 0
+# T : Maturity
+# sigma : Volatility of the underlying asset
+# r : Risk-free rate                             
+
+
 BlackScholesEuPut <- function(S0,K,T,sigma,r){
-  d1 <- (log(S0/K) + (r + 0.5*sigma^2)*T)/(sigma*sqrt(T))
-  d2 <- d1 - sigma*sqrt(T)
-  C <- K*exp(-r*T)*pnorm(-d2) - S0*pnorm(-d1)
-  return(C)
+    
+  # d1 and d2 are parameters of the Black-Scholes formula
+  d1 = (log(S0/K) + (r + 0.5*sigma^2)*T)/(sigma*sqrt(T))
+  d2 = d1 - sigma*sqrt(T)
+  
+  # Calculate the price of the European option (P) using the Black-Scholes formula
+  P = K*exp(-r*T)*pnorm(-d2) - S0*pnorm(-d1)
+  return(P)
 }
-BlackScholesEuPut(50,50,5/12,0.4,0.1)
+
+# An example of inputs:
+# BlackScholesEuPut(50,50,5/12,0.4,0.1)
 
